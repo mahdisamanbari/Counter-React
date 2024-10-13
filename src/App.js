860c0c1001faa04dd85d142cbb6d8373f9b4868a
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-10-13 19:24:17 +0330

    counter css added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  
+  
+  
+
+  return (
+    <>
+           <button  className='btn'>+</button>
+           <button  className='btn'>-</button>
+           <h2></h2>
+    </>
+  );
+};
+
+export default App;
