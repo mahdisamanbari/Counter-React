8bd60edcb8afae2c9a740daca6c3b231368b22d7
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-10-13 19:39:43 +0330

    counter react project done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  
-  
+  const [count,setCount]=useState(0)
+  const increment = ()=>{
+    setCount(count + 1)
+  }
+  const decrement = ()=>{
+    setCount(count - 1)
+  }
   
 
   return (
-    <>
-           <button  className='btn'>+</button>
-           <button  className='btn'>-</button>
-           <h2></h2>
-    </>
+    <div className='app'>
+           <button onClick={()=>increment()} className='btn'>+</button>
+           <button onClick={()=>decrement()}  className='btn'>-</button>
+           <h2 className='counter-text'>{count}</h2>
+    </div>
   );
 };
 
